28b189bace47b584058c9362f160116dedf46b81
MadMan <63459591+MadMan-123@users.noreply.github.com>
2022-10-26 10:20:14 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,192 +1,56 @@
-﻿using SFML.Graphics;
+﻿using Wolstencroft;
+using SFML.Graphics;
 using SFML.System;
 using SFML.Audio;
 using SFML.Window;
 
-
-namespace Test
+class Program
 {
-    
-    class Component
+    class PlayerController : Component
     {
-        protected Game game;
-        public Entity entity{get; private set;}
-
-        public virtual void OnUpdate() {}
-        public virtual void OnStart() {}
-
-        public Component()
-        {
-            game = Game.Instance;
-        }
-
-        public void SetEntityParent(ref Entity entity)
-        {
-            this.entity = entity;
-        }
-
-    }
-
-    class Renderable : Component
-    {
-        RectangleShape Body = new RectangleShape();
-
+        float fAxisY = 0;
+        float fSpeed = 2f;
         public override void OnUpdate()
         {
-            Body.Position = entity.GetComponent<Transform2D>().position;
-            Game.Instance.Draw(Body);
+            if (Game.Instance.CurrentPressedKey == SFML.Window.Keyboard.Key.W)
+            {
+                fAxisY += .001f;
+            }
+            else if (Game.Instance.CurrentPressedKey == SFML.Window.Keyboard.Key.S)
+            {
+                fAxisY -= .001f;
+            }
+
+            fAxisY = Math.Clamp(fAxisY, -1, 1);
+
+            //move the player towards the mouse
+
+            //get direction
+            Vector2f _vDir = entity.transform.position - (Vector2f)Mouse.GetPosition();
+
+            //get normal
+            Vector2f _vDirNorm = new Vector2f(
+                _vDir.X / MathF.Sqrt(MathF.Pow(_vDir.X, 2)) + MathF.Pow(_vDir.X, 2),
+                _vDir.Y / MathF.Sqrt(MathF.Pow(_vDir.Y, 2) + MathF.Pow(_vDir.Y, 2))
+                );
+
+            Game.Instance.Log(_vDirNorm);
+
+           entity.transform.position += new Vector2f();
         }
     }
 
-    class Transform2D : Component
+
+    static Game game = new Game();
+    static void Main()
     {
-        public Vector2f position = new Vector2f(0,0);
-        public float fRotation = 0f;
-    }
+        Entity Player = new Entity();
+        Player.AddComponent<PlayerController>();
+        game.AddEntity(Player);
+        game.Start();
 
-    class Entity
-    {
-        public List<Component> components = new List<Component>();
-        
-        private Entity entityRef;
-
-        public Entity()
-        {
-            entityRef = this;
-            //add transform2d by default
-            AddComponent<Transform2D>();
-        }
-
-        public void RunStarts()
-        {
-             foreach(var Comp in components)
-            {
-                Comp.OnStart();
-            }
-        }
-        public void RunUpdates()
-        {
-            //runs all component updates 
-            foreach(var Comp in components)
-            {
-                Comp.OnUpdate();
-            }
-        }
-
-        public void AddComponent<T>() where T: Component, new()
-        {
-            T NewComp = new T();
-            NewComp.SetEntityParent(ref entityRef);
-            components.Add(NewComp);
-        }
-
-        public T GetComponent<T>() where T: Component, new()
-        {
-            T NewComp = new T();
-            
-            foreach(var Comp in components)
-            {
-                if(NewComp.GetType() == Comp.GetType())
-                {
-                    return (T)Comp;
-                }
-            }
-
-            return null;
+    }   
 
 
-        }
+};
 
-
-    };
-
-    class Game
-    {
-       
-
-        public static Game Instance = null;
-
-        public RenderWindow window;
-
-        public uint iWidth = 200, iHeight = 200;
-
-        public string sName = "Window";
-        List<Entity> Entitys = new List<Entity>();
-
-
-        public Game() 
-        {             
-            if(Instance == null)
-                Instance = this;
-            window = new RenderWindow(new VideoMode(iWidth,iHeight),sName);
-        }
-
-        public void Draw(SFML.Graphics.Drawable drawable)
-        {
-            if(window  != null || drawable != null)
-                window.Draw(drawable);
-        }
-
-
-        public void Start()
-        {
-            window.Closed += HandleClose;
-        
-            HandleUpdate();
-        }
-
-        void HandleUpdate()
-        {
-
-
-            while(window.IsOpen)
-            {
-                window.DispatchEvents();
-                window.Clear();
-                
-                //call all entity updates
-                HandleEntityUpdates();
-
-                window.Display();
-            }
-        }
-        void HandleClose(object sender, EventArgs e)
-        {
-            window.Close();
-            window.Closed -= HandleClose;
-        }
-        void HandleEntityUpdates()
-        {
-            foreach(var Ent in Entitys)
-            {
-                Ent.RunUpdates();
-            }
-        }
-
-        public void AddEntity(Entity entity)
-        {
-            entity.RunStarts();
-            Entitys.Add(entity);
-            
-        }
-        
-    };
-
-
-
-    
-
-    class Program
-    {
-        
-
-            
-        static Game g = new Game();
-        static void Main()
-        {
-            g.Start();
-        }   
-
-
-    };
-}
